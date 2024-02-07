b56ddb5a5e8f77dd84c13dcca98ee102c6e04138
Mateo Polanco Rodriguez <mateopolanco2@gmail.com>
2024-02-07 12:50:41 -0500

    Sincronizo cambios

---
+++ WebAppAsesorias/Views/ClasificacionPeticion/registroDePeticionarios.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace asesorias.Views.Home
+{
+    public class registroDePeticionariosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
