ec5825036429c1b61315fb7c9fd4a066eaadb6a5
Jose Alvaro hincapie Castillo <alvarohincapie@gmail.com>
2024-02-08 10:19:14 -0500

    Se crean clases en las capas para los parámetros

---
+++ Logica/Logica/ParametroLogica.cs
@@ -0,0 +1,30 @@
+﻿using AccesoDatos.Data;
+using Logica.dto;
+using System;
+using System.Collections.Generic;
+
+namespace Logica
+{
+    public class ParametroLogica
+    {
+        public List<ParametroDTO> consultarParametrosXCodTipo(string codTipoParametro, System.Data.DataTable dataTable)
+        {
+            ParametroData parametroData = new ParametroData();
+            var data = parametroData.consultarParametrosXCodTipo(codTipoParametro);
+
+            var lista = new List<ParametroDTO>();
+
+            /*
+            foreach (var item in data)
+            {
+                
+            }
+            */
+            return lista;
+        }
+        public static void main(String[] args)
+        {
+            Console.WriteLine("Hola mundo");
+        }
+    }
+}
---
+++ Logica/dto/ParametroDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica.dto
+{
+    public class ParametroDTO
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Descripcion { get; set; }        
+    }
+}
